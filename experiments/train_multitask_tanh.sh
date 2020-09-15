02a3429d44575458077e17c94e280848f9d3d5b1
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2020-09-15 10:19:57 -0300

    Update experiments

--- experiments/train_multitask_tanh.sh
+++ experiments/train_multitask_tanh.sh
@@ -2,12 +2,12 @@
 
 python run.py \
     --gpus=1 \
-    --learning_rate=0.0005 \
+    --learning_rate=0.001 \
     --epochs=500 \
     --batch_size=32 \
     --dataset_path=./datasets/worksites.csv \
     --regression_task=true \
-    --fill_missing_regression=3650 \
+    --fill_missing_regression=1460 \
     --classification_task=true \
     --input_size=24 \
     --hidden_size=50 \
--- experiments/train_regression_filling.sh
+++ experiments/train_regression_filling.sh
@@ -2,12 +2,12 @@
 
 python run.py \
     --gpus=1 \
-    --learning_rate=0.0005 \
-    --epochs=500 \
+    --learning_rate=0.001 \
+    --epochs=200 \
     --batch_size=32 \
     --dataset_path=./datasets/worksites.csv \
     --regression_task=true \
-    --fill_missing_regression=3650 \
+    --fill_missing_regression=1460 \
     --input_size=24 \
     --hidden_size=50 \
     --deterministic=true \
